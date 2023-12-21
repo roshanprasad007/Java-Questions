ba0eb776861eee9f4287036d2f2a4b0258e41167
roshan prasad <roshanprasadcse@mail.com>
2023-12-21 15:14:41 +0530

    Second program

---
+++ Message.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Message {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a name");
+        String  name=sc.next();
+        System.out.println("Hello, How are you! "+name);
+
+    }
+}
